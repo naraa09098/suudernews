d8a05bfa0211bba135f11626b07ec119f7b3bdca
Narmandakh Maamkhuu <narmandakh@must.edu.mn>
2026-04-07 09:39:10 +0800

    fix firebase config

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -3,38 +3,43 @@ export default async function handler(req, res) {
     const base = "https://suudernews.mn"
 
     const response = await fetch(
-        "https://firestore.googleapis.com/v1/projects/YOUR_PROJECT_ID/databases/(default)/documents/news"
+        "https://firestore.googleapis.com/v1/projects/suudernews/databases/(default)/documents/news"
     )
 
     const data = await response.json()
 
-    const news = data.documents || []
+    const docs = data.documents || []
 
-    const urls = news.map(doc => {
-        const fields = doc.fields
+    let urls = ""
+
+    docs.forEach(doc => {
 
         const id = doc.name.split("/").pop()
 
-        const slug =
-            fields.slug?.stringValue || id
+        const fields = doc.fields || {}
+
+        const slug = fields.slug?.stringValue || id
 
         const date =
             fields.createdAt?.timestampValue ||
             new Date().toISOString()
 
-        return `
+        urls += `
 <url>
 <loc>${base}/news/${slug}</loc>
 <lastmod>${date}</lastmod>
+<priority>0.8</priority>
 </url>
 `
-    }).join("")
+
+    })
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
 <url>
-<loc>${base}</loc>
+<loc>${base}/</loc>
+<priority>1.0</priority>
 </url>
 
 ${urls}
@@ -43,4 +48,5 @@ ${urls}
 
     res.setHeader("Content-Type", "text/xml")
     res.send(xml)
+
 }
