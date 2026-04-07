37b9acd5d20c50e9409fc7b79693f0321333793c
Narmandakh Maamkhuu <narmandakh@must.edu.mn>
2026-04-07 10:21:38 +0800

    add base seo meta

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
+import prerender from 'vite-plugin-prerender'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+    prerender({
+      routes: [
+        '/',
+        '/category/uls-tur',
+        '/category/niigem',
+        '/category/ediin-zasag',
+        '/category/niitlel',
+        '/category/nevtruuleg'
+      ]
+    })
+  ]
 })
