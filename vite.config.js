2658b893f68c093678bcdd9e1eb9ed0a87b296ad
Narmandakh Maamkhuu <narmandakh@must.edu.mn>
2026-04-07 10:27:16 +0800

    refresh node modules

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from 'vite-plugin-prerender'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      routes: [
-        '/',
-        '/category/uls-tur',
-        '/category/niigem',
-        '/category/ediin-zasag',
-        '/category/niitlel',
-        '/category/nevtruuleg'
-      ]
-    })
-  ]
+  plugins: [react()]
 })
