ad7a3a4b60a7f76a2718d2922decf1ab91bb03bd
Narmandakh Maamkhuu <narmandakh@must.edu.mn>
2026-04-07 09:40:40 +0800

    fix sitemap fetch

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -1,52 +1,49 @@
 export default async function handler(req, res) {
-
     const base = "https://suudernews.mn"
 
-    const response = await fetch(
-        "https://firestore.googleapis.com/v1/projects/suudernews/databases/(default)/documents/news"
-    )
+    try {
+        const response = await fetch(
+            "https://firestore.googleapis.com/v1/projects/suudernews/databases/(default)/documents/news?pageSize=100"
+        )
 
-    const data = await response.json()
+        const data = await response.json()
 
-    const docs = data.documents || []
+        const docs = data.documents || []
 
-    let urls = ""
+        const urls = docs.map(doc => {
+            const id = doc.name.split("/").pop()
+            const fields = doc.fields || {}
 
-    docs.forEach(doc => {
+            const slug = fields.slug?.stringValue || id
 
-        const id = doc.name.split("/").pop()
+            const date =
+                fields.createdAt?.timestampValue ||
+                new Date().toISOString()
 
-        const fields = doc.fields || {}
+            return `
+      <url>
+        <loc>${base}/news/${slug}</loc>
+        <lastmod>${date}</lastmod>
+        <priority>0.8</priority>
+      </url>`
+        }).join("")
 
-        const slug = fields.slug?.stringValue || id
+        const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-        const date =
-            fields.createdAt?.timestampValue ||
-            new Date().toISOString()
+      <url>
+        <loc>${base}</loc>
+        <priority>1.0</priority>
+      </url>
 
-        urls += `
-<url>
-<loc>${base}/news/${slug}</loc>
-<lastmod>${date}</lastmod>
-<priority>0.8</priority>
-</url>
-`
+      ${urls}
 
-    })
+    </urlset>`
 
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        res.setHeader("Content-Type", "text/xml")
+        res.status(200).send(xml)
 
-<url>
-<loc>${base}/</loc>
-<priority>1.0</priority>
-</url>
-
-${urls}
-
-</urlset>`
-
-    res.setHeader("Content-Type", "text/xml")
-    res.send(xml)
-
+    } catch (e) {
+        res.status(500).send("sitemap error")
+    }
 }
