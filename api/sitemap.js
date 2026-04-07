b4bf688a15e1125734c2c0e6791ab2113ca8ac63
Narmandakh Maamkhuu <narmandakh@must.edu.mn>
2026-04-07 09:46:51 +0800

    fix sitemap final

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,35 +1,41 @@
 export default async function handler(req, res) {
-    const base = "https://suudernews.mn"
+    try {
+        const base = "https://suudernews.mn";
 
-    const r = await fetch(
-        "https://firestore.googleapis.com/v1/projects/suudernews/databases/(default)/documents/news"
-    )
+        const response = await fetch(
+            "https://firestore.googleapis.com/v1/projects/suudernews/databases/(default)/documents/news"
+        );
 
-    const json = await r.json()
-    const docs = json.documents || []
+        const data = await response.json();
+        const docs = data.documents || [];
 
-    let urls = ""
+        let urls = "";
 
-    docs.forEach(doc => {
-        const id = doc.name.split("/").pop()
-        const f = doc.fields || {}
+        docs.forEach((doc) => {
+            const id = doc.name.split("/").pop();
+            const fields = doc.fields || {};
 
-        const slug = f.slug?.stringValue || id
+            const slug = fields.slug?.stringValue || id;
 
-        urls += `
-    <url>
-      <loc>${base}/news/${slug}</loc>
-    </url>`
-    })
+            urls += `
+        <url>
+          <loc>${base}/news/${slug}</loc>
+        </url>
+      `;
+        });
 
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>${base}</loc>
-    </url>
-    ${urls}
-  </urlset>`
+        const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${base}</loc>
+  </url>
+  ${urls}
+</urlset>`;
 
-    res.setHeader("Content-Type", "text/xml")
-    res.send(xml)
+        res.setHeader("Content-Type", "text/xml");
+        res.status(200).send(xml);
+
+    } catch (error) {
+        res.status(500).send("sitemap error");
+    }
 }
