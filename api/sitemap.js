8fa7949e4242ed749cb4b9ff2716844dd3e0d809
Narmandakh Maamkhuu <narmandakh@must.edu.mn>
2026-04-07 09:44:58 +0800

    fix sitemap vercel

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-
     const base = "https://suudernews.mn"
 
     const r = await fetch(
@@ -7,42 +6,30 @@ export default async function handler(req, res) {
     )
 
     const json = await r.json()
-
     const docs = json.documents || []
 
     let urls = ""
 
-    for (const doc of docs) {
-
+    docs.forEach(doc => {
         const id = doc.name.split("/").pop()
         const f = doc.fields || {}
 
         const slug = f.slug?.stringValue || id
 
-        const date =
-            f.createdAt?.timestampValue ||
-            new Date().toISOString()
-
         urls += `
-<url>
-<loc>${base}/news/${slug}</loc>
-<lastmod>${date}</lastmod>
-</url>
-`
-    }
+    <url>
+      <loc>${base}/news/${slug}</loc>
+    </url>`
+    })
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
-<url>
-<loc>${base}/</loc>
-</url>
-
-${urls}
-
-</urlset>`
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${base}</loc>
+    </url>
+    ${urls}
+  </urlset>`
 
     res.setHeader("Content-Type", "text/xml")
     res.send(xml)
-
 }
